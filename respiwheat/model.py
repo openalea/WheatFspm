4efe3e9083ab53ce850d4252a48f30cf85a4c346
Romain Barillot <22076811+rbarillot@users.noreply.github.com>
2019-07-17 13:32:21 +0000

    Merge develop with ./tillering_hack

--- respiwheat/model.py
+++ respiwheat/model.py
@@ -28,8 +28,7 @@ class RespirationModel(object):
     SECOND_TO_HOUR_RATE_CONVERSION = 3600
 
     # R_growth#
-    YG = 0.8             # Growth yield (units of C appearing in new biomass per unit of C substrate utilized for growth)
-                            # range 0±75 to 0±85 in Cannell and Thornley, 2000.
+    YG = 0.8               # Growth yield (units of C appearing in new biomass per unit of C substrate utilized for growth). Range 0±75 to 0±85 in Cannell and Thornley, 2000.
 
     YG_GRAINS = 0.71       # Growth yield (units of C appearing in new biomass per unit of C substrate utilized for growth)
 
@@ -59,36 +58,26 @@ class RespirationModel(object):
 
     @classmethod
     def R_growth(cls, mstruct_growth):
-        """
-        Local growth respiration
+        """ Local growth respiration
 
-        : Parameters:
-            - `mstruct_growth` (:class:`float`) - gross growth of mstruct (µmol C added in mstruct)
+        :param float mstruct_growth: gross growth of mstruct (µmol C added in mstruct)
 
-        : Returns:
-            _R_growth (µmol C respired)
-
-        :Returns Type:
-            :class:`float`
+        :return: R_growth (µmol C respired)
+        :rtype: float
         """
         R_growth = ((1 - cls.YG) / cls.YG) * mstruct_growth
         return R_growth
 
     @classmethod
     def R_grain_growth(cls, mstruct_growth, starch_filling, mstruct):
-        """
-        Grain growth respiration
+        """ Grain growth respiration
 
-        : Parameters:
-            - `mstruct_growth` (:class:`float`) - gross growth of grain structure (µmol C added in grain structure)
-            - `starch_filling` (:class:`float`) - gross growth of grain starch (µmol C added in grain starch g-1 mstruct)
-            - `mstruct` (:class:`float`) -  structural dry mass of organ (g)
+        :param float mstruct_growth: gross growth of grain structure (µmol C added in grain structure)
+        :param float starch_filling: gross growth of grain starch (µmol C added in grain starch g-1 mstruct)
+        :param float mstruct: structural dry mass of organ (g)
 
-        : Returns:
-            R_grain_growth (µmol C respired)
-
-        :Returns Type:
-            :class:`float`
+        :return: R_grain_growth_struct, R_grain_growth_starch (µmol C respired)
+        :rtype: (float, float)
         """
         R_grain_growth_struct = ((1 - cls.YG_GRAINS)/cls.YG_GRAINS) * mstruct_growth
         R_grain_growth_starch = ((1 - cls.YG_GRAINS)/cls.YG_GRAINS) * (starch_filling*mstruct)
@@ -96,145 +85,108 @@ class RespirationModel(object):
 
     @classmethod
     def R_phloem(cls, sucrose_loading, mstruct):
+        """ Phloem loading respiration
+
+        :param float sucrose_loading: Loading flux from the C substrate pool to phloem (µmol C g-1 mstruct)
+        :param float mstruct: structural dry mass of organ (g)
+
+        :return: R_phloem, sucrose_loading (µmol C respired, µmol C)
+        :rtype: (float, float)
         """
-        Phloem loading respiration
-
-        : Parameters:
-            - `sucrose_loading` (:class:`float`) -  Loading flux from the C substrate pool to phloem (µmol C g-1 mstruct)
-            - `sucrose` (:class:`float`) -  amount of C sucrose in organ (µmol C)
-            - `mstruct` (:class:`float`) -  structural dry mass of organ (g)
-
-        : Returns:
-            _R_phloem (µmol C respired)
-
-        :Returns Type:
-            :class:`float`
-        """
-        _R_phloem = max(0, cls.CPHLOEM * sucrose_loading * mstruct)  #: Do not count a respiratory cost for negative loading i.e. unloading (assumed to be passive)
-        return _R_phloem, sucrose_loading
+        R_phloem = max(0., cls.CPHLOEM * sucrose_loading * mstruct)  #: Do not count a respiratory cost for negative loading i.e. unloading (assumed to be passive)
+        return R_phloem, sucrose_loading
 
     @classmethod
     def R_Namm_upt(cls, U_Namm):
-        """
-        Ammonium uptake respiration
+        """ Ammonium uptake respiration
 
-        : Parameters:
-            - `U_Namm` (:class:`float`) -  uptake of N ammonium (µmol N)
+        :param float U_Namm: uptake of N ammonium (µmol N)
 
-        : Returns:
-            _R_Namm (µmol C respired)
-
-        :Returns Type:
-            :class:`float`
+        :return: R_Namm (µmol C respired)
+        :rtype: float
         """
         R_Namm_upt = cls.C_AMM_UPT * U_Namm
         return R_Namm_upt
 
     @classmethod
     def R_Nnit_upt(cls, U_Nnit, sucrose):
-        """
-        Nitrate uptake respiration
+        """ Nitrate uptake respiration
 
-        : Parameters:
-            - `U_Nnit` (:class:`float`) - uptake of N nitrates (µmol N)
-            - `sucrose` (:class:`float`) -  amount of C sucrose in organ (µmol C)
+        :param float U_Nnit: uptake of N nitrates (µmol N)
+        :param float sucrose: amount of C sucrose in organ (µmol C)
 
-        : Returns:
-            _R_Nnit_upt (µmol C respired)
-
-        :Returns Type:
-            :class:`float`
+        :return: R_Nnit_upt (µmol C respired)
+        :rtype: float
         """
         if sucrose > 0:
-            _R_Nnit_upt = cls.C_NIT_UPT * U_Nnit
+            R_Nnit_upt = cls.C_NIT_UPT * U_Nnit
         else:
-            _R_Nnit_upt = 0
-        return _R_Nnit_upt
+            R_Nnit_upt = 0
+        return R_Nnit_upt
 
     @classmethod
     def R_Nnit_red(cls, s_amino_acids, sucrose, mstruct, root=False):
-        """
-        Nitrate reduction-linked respiration
+        """ Nitrate reduction-linked respiration
         Distinction is made between nitrate realised in roots or in shoots where a part of the energy required is derived from ATP
         and reducing power obtained directly from photosynthesis (rather than C substrate)
 
-        : Parameters:
-            - `s_amino_acids` (:class:`float`) - consumption of N for the synthesis of amino acids (µmol N g-1 mstruct)
-              (in the present version, this is used to approximate nitrate reduction needed in the original model of Thornley and Cannell, 2000)
-            - `sucrose` (:class:`float`) -  amount of C sucrose in organ (µmol C)
-            - `mstruct` (:class:`float`) -  structural dry mass of organ (g)
-            - `root` (:class:`bool`) - specifies if the nitrate reduction-linked respiration is computed for shoot (False) or root (True) tissues.
+        :param float s_amino_acids: consumption of N for the synthesis of amino acids (µmol N g-1 mstruct)
+        (in the present version, this is used to approximate nitrate reduction needed in the original model of Thornley and Cannell, 2000)
+        :param float sucrose: amount of C sucrose in organ (µmol C)
+        :param float mstruct: structural dry mass of organ (g)
+        :param bool root: specifies if the nitrate reduction-linked respiration is computed for shoot (False) or root (True) tissues.
 
-        : Returns:
-            _R_Nnit_upt (µmol C respired)
-
-        :Returns Type:
-            :class:`float`
+        :return: R_Nnit_upt, s_amino_acids (µmol C respired, µmol N g-1 mstruct)
+        :rtype: (float, float)
         """
         if not root:
-            _R_Nnit_red = cls.F_NIT_RED_SH_CS * cls.C_NIT_RED * s_amino_acids * mstruct  # Respiration in shoot tissues
+            R_Nnit_red = cls.F_NIT_RED_SH_CS * cls.C_NIT_RED * s_amino_acids * mstruct  # Respiration in shoot tissues
         else:
-            _R_Nnit_red = cls.C_NIT_RED * s_amino_acids * mstruct                         # Respiration in root tissues
+            R_Nnit_red = cls.C_NIT_RED * s_amino_acids * mstruct                         # Respiration in root tissues
 
-#        if sucrose < _R_Nnit_red:
-#            _R_Nnit_red = 0
+#        if sucrose < R_Nnit_red:
+#            R_Nnit_red = 0
 #            s_amino_acids = 0
-        return _R_Nnit_red, s_amino_acids
+        return R_Nnit_red, s_amino_acids
 
     @classmethod
     def R_N2fix(cls, I_Nfix):
+        """ N2-fixation respiration
+
+        :param float I_Nfix: flux of fixed N into the root substrate N pool (kg fixed N)
+
+        :return: R_N2fix (µmol C respired)
+        :rtype: float`
         """
-        N2-fixation respiration
-
-        : Parameters:
-            - `I_Nfix` (:class:`float`) - flux of fixed N into the root substrate N pool (kg fixed N)
-
-        : Returns:
-            _R_N2fix (µmol C respired)
-
-        :Returns Type:
-            :class:`float`
-        """
-
-        _R_N2fix = cls.C_NFIX * I_Nfix
-        return _R_N2fix
+        R_N2fix = cls.C_NFIX * I_Nfix
+        return R_N2fix
 
     @classmethod
     def R_min_upt(cls, delta_BMstruct):
-        """
-        Mineral ion (other than N) uptake-linked respiration
+        """ Mineral ion (other than N) uptake-linked respiration
 
-        : Parameters:
-            - `delta_BMstruct` (:class:`float`) - gross plant structural growth rate (g)
+        :param float delta_BMstruct: gross plant structural growth rate (g)
 
-        : Returns:
-            _R_min_upt (µmol C respired)
-
-        :Returns Type:
-            :class:`float`
+        :return: R_min_upt (µmol C respired)
+        :rtype float
         """
         # Uptake of minerals (g)
         Umin = (cls.ASHE_CONTENT * delta_BMstruct) * 0.5  # 0.5: minerals internally recycled from senescing leaves
         # Respiratory cost
-        _R_min_upt = cls.CMIN_UPT * Umin
-        return _R_min_upt
+        R_min_upt = cls.CMIN_UPT * Umin
+        return R_min_upt
 
     @classmethod
     def R_residual(cls, sucrose, mstruct, Ntot, Ts):
-        """
-        Residual maintenance respiration (cost from protein turn-over, cell ion gradients, futile cycles...)
+        """ Residual maintenance respiration (cost from protein turn-over, cell ion gradients, futile cycles...)
 
-        : Parameters:
-            - `sucrose` (:class:`float`) - amount of C sucrose (µmol C)
-            - `mstruct` (:class:`float`) - structural dry mass of organ (g)
-            - `Ntot` (:class:`float`) - total N in organ (µmol N)
-            - `Ts` (:class:`float`) - organ temperature (°C)
+        :param float sucrose: amount of C sucrose (µmol C)
+        :param float mstruct: structural dry mass of organ (g)
+        :param float Ntot: total N in organ (µmol N)
+        :param float Ts : organ temperature (°C)
 
-        : Returns:
-            _R_residual (µmol C respired h-1)
-
-        :Returns Type:
-            :class:`float`
+        :return: R_residual, R_maintenance (µmol C respired h-1)
+        :rtype: (float, float)
         """
         conc_sucrose = sucrose / mstruct
         Q10 = 2
--- test/test_respiwheat.py
+++ test/test_respiwheat.py
@@ -1,4 +1,7 @@
 # -*- coding: latin-1 -*-
+
+from respiwheat import model
+
 """
     test_respiwheat
     ~~~~~~~~~~~~~~~
@@ -27,15 +30,15 @@
         $Id$
 """
 
-from respiwheat import model
 
 def assert_close(actual, desired, tolerance=0.01):
     assert abs(actual - desired) < tolerance * (abs(desired) + 1)
 
+
 def test_calculate_respiwheat():
 
-    mstruct_growth =10
-    starch_filling =10
+    mstruct_growth = 10
+    starch_filling = 10
     sucrose_loading = 10
     U_Namm = 10
     U_Nnit = 10
@@ -45,27 +48,21 @@ def test_calculate_respiwheat():
     sucrose = 10
     Ntot = 10
     mstruct = 1
-    delta_t= 3600
-    Ts= 20
+    Ts = 20
 
-    actual_respirations = {}
+    actual_respirations = {'R_growth': model.RespirationModel.R_growth(mstruct_growth), 'R_grain_growth_struct': model.RespirationModel.R_grain_growth(mstruct_growth, starch_filling, mstruct)[0],
+                           'R_grain_growth_starch': model.RespirationModel.R_grain_growth(mstruct_growth, starch_filling, mstruct)[1],
+                           'R_phloem': model.RespirationModel.R_phloem(sucrose_loading, mstruct)[0], 'R_Namm': model.RespirationModel.R_Namm_upt(U_Namm),
+                           'R_Nnit': model.RespirationModel.R_Nnit_upt(U_Nnit, sucrose), 'R_Nnit_red_shoot': model.RespirationModel.R_Nnit_red(s_amino_acids, sucrose, mstruct)[0],
+                           'R_Nnit_red_roots': model.RespirationModel.R_Nnit_red(s_amino_acids, sucrose, mstruct, root=True)[0], 'R_N2fix': model.RespirationModel.R_N2fix(I_Nfix),
+                           'R_min_upt': model.RespirationModel.R_min_upt(delta_BMstruct), 'R_residual': model.RespirationModel.R_residual(sucrose, mstruct, Ntot, Ts)[0]}
 
-    actual_respirations['R_growth'] =  model.RespirationModel.R_growth(mstruct_growth)
-    actual_respirations['R_grain_growth_struct'], actual_respirations['R_grain_growth_starch'] =  model.RespirationModel.R_grain_growth(mstruct_growth, starch_filling, mstruct)
-    actual_respirations['R_phloem'] =  model.RespirationModel.R_phloem(sucrose_loading, mstruct)[0]
-    actual_respirations['R_Namm'] =  model.RespirationModel.R_Namm_upt(U_Namm)
-    actual_respirations['R_Nnit'] =  model.RespirationModel.R_Nnit_upt(U_Nnit, sucrose)
-    actual_respirations['R_Nnit_red_shoot'] =  model.RespirationModel.R_Nnit_red(s_amino_acids, sucrose, mstruct)[0]
-    actual_respirations['R_Nnit_red_roots'] =  model.RespirationModel.R_Nnit_red(s_amino_acids, sucrose, mstruct, root = True)[0]
-    actual_respirations['R_N2fix'] =  model.RespirationModel.R_N2fix(I_Nfix)
-    actual_respirations['R_min_upt'] =  model.RespirationModel.R_min_upt(delta_BMstruct)
-    actual_respirations['R_residual'] =  model.RespirationModel.R_residual(sucrose, mstruct, Ntot, Ts)[0]
+    desired_respirations = {'R_growth': 2.5, 'R_grain_growth_struct': 4.085, 'R_grain_growth_starch':  4.085, 'R_phloem': 0.06, 'R_Namm': 1.98, 'R_Nnit': 3.97, 'R_Nnit_red_shoot': 9.9,
+                            'R_Nnit_red_roots': 19.8, 'R_N2fix': 60, 'R_min_upt': 1250, 'R_residual': 0.00088}
 
-    desired_respirations = {'R_growth' : 2.5, 'R_grain_growth_struct' : 4.085, 'R_grain_growth_starch' :  4.085, 'R_phloem' : 0.06, 'R_Namm' : 1.98, 'R_Nnit' : 3.97, 'R_Nnit_red_shoot' : 9.9,
-                            'R_Nnit_red_roots' : 19.8, 'R_N2fix' : 60, 'R_min_upt' : 1250, 'R_residual' : 0.00088}
-
-    for R, desired_R in desired_respirations.iteritems():
+    for R, desired_R in desired_respirations.items():
         assert_close(actual_respirations[R], desired_R, tolerance=1e-3)
+
 
 if __name__ == '__main__':
     test_calculate_respiwheat()
